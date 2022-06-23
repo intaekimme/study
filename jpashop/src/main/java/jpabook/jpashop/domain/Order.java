b896a3a8b46c955e7c589d38c6d92cc859da75fb
InTae <kit938639@gmail.com>
2022-06-24 01:56:14 +0900

    Upload 엔티티 클래스 개발 1,2

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne
+    @JoinColumn(name = "delivery")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderSatus satus;   //  주문 상태 [ORDER, CANCLE]
+}
---
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderSatus.java
@@ -0,0 +1,5 @@
+package jpabook.jpashop.domain;
+
+public enum OrderSatus {
+    ORDER, CANCLE
+}
