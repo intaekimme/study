2d274df775204477c72e8917f655cd7bf84545e1
InTae <kit938639@gmail.com>
2022-06-23 23:31:49 +0900

    Upload JPA와 DB 설정, 동작확인

--- jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -1,2 +1,24 @@
-package jpabook.jpashop;public class MemberRepository {
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+//    저장
+    public Long save(Member member){
+        em.persist(member);     //  커맨드와 쿼리 분리
+        return member.getId();
+    }
+
+//    조회
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
 }
