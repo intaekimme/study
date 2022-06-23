b89f2db6945483ff8e5814ab6153c555d667f0d5
InTae <kit938639@gmail.com>
2022-06-24 02:25:27 +0900

    "Upload 엔티티 설계시 주의점"

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -5,6 +5,9 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.CascadeType.ALL;
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Table(name = "orders")
 public class Order {
@@ -13,13 +16,13 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne @JoinColumn(name = "member_id")
+    @ManyToOne(fetch = LAZY) @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = ALL)
     @JoinColumn(name = "delivery")
     private Delivery delivery;
 
@@ -27,4 +30,19 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderSatus satus;   //  주문 상태 [ORDER, CANCLE]
+
+    //==연관관계 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
 }
