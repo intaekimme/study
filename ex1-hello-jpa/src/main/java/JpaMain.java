6f0400c8839063b772ca13cd75c29c05cc35a54c
InTae <kit938639@gmail.com>
2022-07-01 02:21:45 +0900

    Upload JPA 시작하기, Hello JPA - 애플리케이션 개발 마무리

--- ex1-hello-jpa/src/main/java/JpaMain.java
+++ ex1-hello-jpa/src/main/java/JpaMain.java
@@ -2,6 +2,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -13,10 +14,11 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setId(2L);
-            member.setName("HelloB");
-            em.persist(member);
+//            Member findMember = em.find(Member.class, 1L);
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
 
             tx.commit();
         } catch (Exception e) {
@@ -24,7 +26,7 @@ public class JpaMain {
         } finally {
             em.close();
         }
-        
+
         emf.close();
     }
 }
